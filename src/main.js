768255c7ee251a734c52e25d7ed638cf3830c25b
StanislavJS <stasyk55@gmail.com>
2025-05-16 22:53:42 +0300

    gallery.css

--- src/main.js
+++ src/main.js
@@ -1,62 +1,61 @@
-import { getImagesByQuery } from "./js/pixabay-api";
+import { getImagesByQuery } from './js/pixabay-api';
 import {
-    createGallery,
-    showLoader,
-    hideLoader,
-} from './js//render-functions';
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions';
 
-import IziToast from 'izitoast';
+import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const formElem = document.querySelector('.form');
-const inputElem = document.querySelector('.input[name="search-text"]');
+const inputElem = formElem.querySelector('input[name="search-text"]');
 
 formElem.addEventListener('submit', findImages);
 
-function findImages(event) { 
-    event.preventDefault();
-    const inputData = inputElem.value.trim().toLowerCase();
+function findImages(event) {
+  event.preventDefault();
+  const inputData = inputElem.value.trim().toLowerCase();
 
-    if (!searchQuery) {
+  if (!inputData) {
+    iziToast.warning({
+      message: 'The input field is empty, try again.',
+      position: 'center',
+    });
+    formElem.reset();
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(inputData)
+    .then(data => {
+      if (data.hits.length === 0) {
         iziToast.warning({
-            message: 'The input field is empty, try again.',
-            position: 'center',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'center',
         });
         formElem.reset();
         return;
-    }
-   
-    clearGallery();
-    showLoader();
+      }
 
+      const images = data.hits;
+      createGallery(images);
 
-    getImagesByQuery(inputData)
-        .then(data => {
-            if (data.hits.length === 0) {
-                iziToast.warning({
-                    message: 'Sorry, there are no images matching your search query. Please try again!',
-                    position: 'center',
-                });
-                formElem.reset();
-                return;
-            }
-
-            const images = data.hits;
-            creatrGallery(images);
-
-            formElem.reset();
-        })
-        .catch(error => {
-          iziToast.warning({
-            message: 'An error occurred. Please try again later.',
-            position: 'center',
-          });
-          console.error(error);
-        })
-        .finally(() => {
-          hideLoader();
-          formElem.reset();
-        });
-    }
-            
-           
+      formElem.reset();
+    })
+    .catch(error => {
+      iziToast.warning({
+        message: 'An error occurred. Please try again later.',
+        position: 'center',
+      });
+      console.error(error);
+    })
+    .finally(() => {
+      hideLoader();
+      formElem.reset();
+    });
+}
